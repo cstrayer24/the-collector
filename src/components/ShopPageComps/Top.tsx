adcee4104eda98867a0a5c4def321354c37e099b
cstrayer24 <cstrayer4303@gmail.com>
2023-08-30 12:26:24 -0400

    jfj

--- src/components/ShopPageComps/Top.tsx
+++ src/components/ShopPageComps/Top.tsx
@@ -1,16 +1,21 @@
 import Link from "next/link";
-
+import Image from "next/image";
+import logo from "../../../imgs/Collector.png";
 function ShopPageTop({ imgUrl }: { imgUrl: string }) {
   return (
-    <div
-      className="w-screen h-72  bg-top bg-cover "
-      style={{ backgroundImage: `url(${imgUrl})` }}
-    >
-      <Link href={"/"} className=" p-4 border-tc-pink border-solid block">
-        <h1 className=" text-center text-white font-bold text-3xl">
-          {" "}
-          The collector
-        </h1>
+    <div className="w-screen h-72  bg-top bg-cover flex justify-center ">
+      <Link
+        href={"/"}
+        className=" p-4 border-tc-pink border-solid block relative right-4]"
+      >
+        <Image
+          placeholder="empty"
+          alt="the collector logo"
+          className=" h-20 rounded-md w-40 "
+          src={logo}
+          width={200}
+          height={100}
+        />
       </Link>
     </div>
   );
